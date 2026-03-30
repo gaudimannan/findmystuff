133817ab1f1beea008f05548dd86dba931aa2998
Mannan Gaudi <mannangaudi60@gmail.com>
2026-03-30 09:41:16 +0530

    Fixed admin photo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -148,8 +148,10 @@ const Navbar = () => {
           />
           <span className="slider"></span>
         </label>
-        <Link to="/profile" className="hidden md:flex w-8 h-8 bg-amber rounded-sm items-center justify-center text-secondary font-bold text-xs hover:brightness-110 transition-all">
-          JD
+        <Link to="/profile" className="hidden md:flex w-8 h-8 bg-amber rounded-sm items-center justify-center text-white hover:brightness-110 transition-all">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
+            <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0zM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695z" clipRule="evenodd" />
+          </svg>
         </Link>
       </div>
     </nav>
